58d2c114fb6f8116866abab7aec347e6e3e057e8
Ian Bachiller <ianbachise@gmail.com>
2024-01-26 11:11:52 -0500

    completes creating and inserting DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.body.remove('main');
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "IAN BACHILLER is the champion";
